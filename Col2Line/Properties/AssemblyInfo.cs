d6ff2a7d92f6eaa9b6a8b337c52a396bab48d3e6
C. Herrera <pro.cah@gmail.com>
2018-05-14 17:58:56 -0500

    Add files via upload

--- Col2Line/Properties/AssemblyInfo.cs
+++ Col2Line/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // conjunto de atributos. Cambie estos valores de atributo para modificar la información
 // asociada con un ensamblado.
 [assembly: AssemblyTitle( "Col2Line" )]
-[assembly: AssemblyDescription( "" )]
+[assembly: AssemblyDescription("Simple Tool to change a Multiple line input to single line")]
 [assembly: AssemblyConfiguration( "" )]
-[assembly: AssemblyCompany( "" )]
+[assembly: AssemblyCompany("NoCorp")]
 [assembly: AssemblyProduct( "Col2Line" )]
 [assembly: AssemblyCopyright( "Copyright ©  2018" )]
 [assembly: AssemblyTrademark( "" )]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // Puede especificar todos los valores o utilizar los números de compilación y de revisión predeterminados
 // mediante el carácter '*', como se muestra a continuación:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion( "1.0.0.0" )]
-[assembly: AssemblyFileVersion( "1.0.0.0" )]
+[assembly: AssemblyVersion("0.0.0.1")]
+[assembly: AssemblyFileVersion("0.0.0.1")]
