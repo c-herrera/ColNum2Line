b92bbae736b97b78fb5d855f6a3903feca6e3400
C. Herrera <pro.cah@gmail.com>
2018-08-29 23:59:13 -0500

    Updated classes
    Created a new about class and changes to UI

--- Col2Line/Col2Line.cs
+++ Col2Line/Col2Line.cs
@@ -4,91 +4,94 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Col2Line
+
+public class ColintoLine
 {
-    class Col2Line
+    private string _singleLine;
+    private string[] _multiLine;
+    private int _totalLines;
+    private List<string> _tempList;
+    private bool _isLineSingle;
+
+
+    /// <summary>
+    /// Class constructor
+    /// </summary>
+    public ColintoLine()
     {
-        private string _singleLine;
-        private string[] _multiLine;
-        private int _totalLines;
-        private List<string> _tempList;
-        private bool _isLineSingle;
+        _singleLine = string.Empty;
+        _totalLines = 0;
+        _tempList = new List<string>();
+    }
 
-        /// <summary>
-        /// Class constructor
-        /// </summary>
-        public Col2Line()
-        {
-            _singleLine = string.Empty;
-            _totalLines = 0;
+    /// <summary>
+    /// Convert a multi line to a single line of text
+    /// </summary>
+    public void ConvertLinesToSingle()
+    {
+        if (_tempList == null)
             _tempList = new List<string>();
-        }
 
-        /// <summary>
-        /// Convert a multi line to a single line of text
-        /// </summary>
-        public void ConvertLinesToSingle()
+        _tempList.Clear();
+
+        _totalLines = 0;
+        foreach (var s in _multiLine)
         {
-            if ( _tempList == null)
-                _tempList = new List<string>();
-
-            _tempList.Clear();
-
-            _totalLines = 0;
-            foreach (var s in _multiLine)
+            if (string.IsNullOrEmpty( s ) == false)
             {
-                if (string.IsNullOrEmpty(s) == false)
-                {
-                    _tempList.Add(s);
-                    _totalLines++;
-                }
-                    
+                _tempList.Add( s );
+                _totalLines++;
             }
-            _multiLine = _tempList.ToArray();
-
-            _singleLine = string.Join( " ", _multiLine );
-            _isLineSingle = true;
 
         }
+        _multiLine = _tempList.ToArray();
 
-        public void ConvertSingleLinetoMultiple()
-        {           
-            if (_singleLine != string.Empty)
-            {
-                _multiLine = _singleLine.Split(' ');
-                _isLineSingle = false;
-            }
-        }
+        _singleLine = string.Join( " ", _multiLine );
+        _isLineSingle = true;
 
-        /// <summary>
-        /// Accesor for the single line
-        /// </summary>
-        public string singleLine
+    }
+
+    public void ConvertSingleLinetoMultiple()
+    {
+        if (_singleLine != string.Empty)
         {
-            get { return _singleLine; }
-            set { _singleLine = value; }
-        }
-
-        /// <summary>
-        /// Accesor for the multiline value
-        /// </summary>
-        public string [] multiLines
-        {
-            set { _multiLine = value; }
-            get { return _multiLine; }
-        }
-
-        /// <summary>
-        /// Total amount of lines
-        /// </summary>
-        public int totalLines
-        {
-            get { return _totalLines; }
-        }
-
-        public bool isLineSingle
-        {
-            get { return _isLineSingle; }
+            _multiLine = _singleLine.Split( ' ' );
+            _isLineSingle = false;
         }
     }
+
+    /// <summary>
+    /// Accesor for the single line
+    /// </summary>
+    public string singleLine
+    {
+        get { return _singleLine; }
+        set { _singleLine = value; }
+    }
+
+    /// <summary>
+    /// Accesor for the multiline value
+    /// </summary>
+    public string[] multiLines
+    {
+        set { _multiLine = value; }
+        get { return _multiLine; }
+    }
+
+    /// <summary>
+    /// Total amount of lines
+    /// </summary>
+    public int totalLines
+    {
+        get { return _totalLines; }
+    }
+
+    /// <summary>
+    /// Checks if the line to convert is single line or not
+    /// </summary>
+    public bool isLineSingle
+    {
+        get { return _isLineSingle; }
+    }
 }
+
